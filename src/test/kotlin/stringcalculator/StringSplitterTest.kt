1237edcd020cfc961376a9e86d062487c2485f94
Jinni01 <dml21@naver.com>
2023-08-06 20:07:20 +0900

    feat: 커스텀 문자열 분리기(Splitter) 테스트 케이스 작성

--- src/test/kotlin/stringcalculator/StringSplitterTest.kt
+++ src/test/kotlin/stringcalculator/StringSplitterTest.kt
@@ -14,4 +14,14 @@ class StringSplitterTest: BehaviorSpec({
             }
         }
     }
+
+    Given("커스텀 구분자 지정 및 해당 구분자로 분할되는 문자열 입력") {
+        val customInput = "//;\n3;2;1"
+        When("입력된 문자열을 인자로 CustomSplitter의 split 메서드를 호출하면") {
+            val split = CustomSplitter.split(customInput)
+            Then("분리된 문자열 목록이 반환된다.") {
+                split shouldBe listOf("1", "2", "3")
+            }
+        }
+    }
 })

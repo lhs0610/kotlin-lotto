767609604b48ad3802a86f7fed44c04dc5b0c914
Jinni01 <dml21@naver.com>
2023-08-06 22:40:57 +0900

    feat: StringToIntConverter 음수 검증 절차 추가

--- src/main/kotlin/stringcalculator/converter/StringToIntConverter.kt
+++ src/main/kotlin/stringcalculator/converter/StringToIntConverter.kt
@@ -1,7 +1,14 @@
 package stringcalculator.converter
 
+import java.lang.IllegalArgumentException
+
 object StringToIntConverter {
+
+    private const val ERROR_INPUT_CANNOT_BE_NEGATIVE = "입력값은 음수일 수 없습니다."
+    private const val MIN_VALUE = 0
     fun convert(input: String): Int {
-        return input.toInt()
+        val value = input.toInt()
+        require(value >= MIN_VALUE) { ERROR_INPUT_CANNOT_BE_NEGATIVE }
+        return value
     }
 }

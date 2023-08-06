70b4312e0d8aa03786aa92624115d39bd336bc42
Jinni01 <dml21@naver.com>
2023-08-06 22:30:34 +0900

    feat: StringCalculatorService 검증식 추가

--- src/main/kotlin/stringcalculator/service/StringCalculateService.kt
+++ src/main/kotlin/stringcalculator/service/StringCalculateService.kt
@@ -3,10 +3,14 @@ package stringcalculator.service
 import stringcalculator.calculator.util.Add
 import stringcalculator.calculator.util.StringCalculator
 import stringcalculator.splitter.StringSplitter
+import stringcalculator.validator.InputValidator
 
 object StringCalculateService {
-    fun sum(input: String): Int {
-        val split = StringSplitter.split(input)
+
+    private const val INTEGER_ZERO = 0
+    fun sum(input: String?): Int {
+        if (!InputValidator.validate(input)) return INTEGER_ZERO
+        val split = StringSplitter.split(input!!)
         return StringCalculator.calculate(split, Add)
     }
 }

58077a3e80421eb07f8731d49a0700cb9a08b887
Jinni01 <dml21@naver.com>
2023-08-06 22:27:42 +0900

    feat: InputValidator 테스트 클래스 작성

---
+++ src/test/kotlin/stringcalculator/InputValidatorTest.kt
@@ -0,0 +1,27 @@
+package stringcalculator
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+
+class InputValidatorTest : BehaviorSpec({
+
+    Given("null 입력") {
+        val input = null
+        When("주어진 입력값을 인자로 InputValidator의 validate 함수를 호출하면") {
+            val validate = InputValidator.validate(input)
+            Then("합을 반환한다") {
+                validate shouldBe false
+            }
+        }
+    }
+
+    Given("공백 문자열 입력") {
+        val input = ""
+        When("주어진 입력값을 인자로 InputValidator의 validate 함수를 호출하면") {
+            val validate = InputValidator.validate(input)
+            Then("합을 반환한다") {
+                validate shouldBe false
+            }
+        }
+    }
+})
